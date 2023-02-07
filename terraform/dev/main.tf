8e17194f10340686765bdadd6e27db79b191d675
EC2 Default User <ec2-user@ip-172-31-12-248.ec2.internal>
2023-02-07 23:44:20 +0000

    Updating security group

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -78,8 +78,26 @@ resource "aws_security_group" "web_sg" {
 
   ingress {
     description      = "HTTP from everywhere"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 8081
+    to_port          = 8081
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 8082
+    to_port          = 8082
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 8083
+    to_port          = 8083
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
